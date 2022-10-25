893d387283d4504381d08df7befcb96a77eea84f
kstark1 <kstark@ualberta.ca>
2022-10-25 17:52:52 -0600

    Test should work

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -12,13 +12,9 @@ import java.util.ArrayList;
 public class CustomListTest {
     private CustomList list;
 
-    @Before
-    public void createList() {
-        list = new CustomList(null, new ArrayList<City>());
-    }
-
     @Test
     public void addCityTest() {
+        list = new CustomList(null, new ArrayList<City>());
         int listSize = list.getCount();
         list.addCity(new City("Estevan", "SK"));
         assertEquals(list.getCount(), listSize + 1);
